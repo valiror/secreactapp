99575d29afc8ea7453affb48758c0c32970e7b90
rockrorvali <meeravalli.sshaik@gmail.com>
2020-01-26 21:39:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,53 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+   render() {
+      return (
+         <div className="App">
+            Hello World!!!
+            <p>Nazu love you</p>
+            <p>{1+1}</p>
+              <Header/>
+            <Content/>
+            <Major/>
+         </div>
+         
+      );
+   }
 }
 
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Header</h1>
+         </div>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>Content</h2>
+            <p>The content text!!!</p>
+         </div>
+      );
+   }
+}
+
+class Major extends React.Component{
+  render(){
+    return(
+    <div className="majorarea">
+      <h4>How are you ! </h4>
+      <p> This my new block.</p>
+    </div>
+    );
+  }
+}
 export default App;
+
+
